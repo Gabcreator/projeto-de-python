64a5dd0f95700eb32a3bc08d37212721c564f711
Gabcreator <Jgabriel.carvalhojf@gmail.com>
2024-11-14 16:34:52 -0300

    atiidades

--- aula14_repetitiva/atividade_005.py/atividade_g.py
+++ aula14_repetitiva/atividade_005.py/atividade_g.py
@@ -3,5 +3,6 @@ import os
 os.system('cls')
 
 numero = int(input('coloque um número: '))
-
-88888
+numero2 = int(input('coloque o segundo número: '))
+for principal in range (numero + numero2):
+    print(f'os primos vão ser {principal}')
---
+++ aula14_repetitiva/atividade_005.py/atividade_h.py
@@ -0,0 +1,20 @@
+# Faça um programa que imprima os valores no intervalo definidos pelo usuário.  Defina 3 números que deverão ser ignorados, ou seja, que não serão impressos na tela.
+import os
+os.system('cls')
+
+numero = int(input('coloque o primeiro número: '))
+numero2 = int(input('coloque o segundo número: '))
+numero3 = int(input('coloque o terceiro número: '))
+
+for numero in range (1, 101):
+    print(f'este número {numero} não está no loop')
+for numero2 in range (1, 101):
+    print(f'este número {numero2} não está no loop')
+for numero3 in range (1, 101):
+    print(f'este número {numero3} não está no loop')
+    if numero in list:
+        print(f'o número {numero} não está no loop')
+    if numero2 in list:
+        print(f'o número {numero2} não está no loop')
+    if numero3 in numero3:
+        print(f'o número {numero3} não está no loop')
