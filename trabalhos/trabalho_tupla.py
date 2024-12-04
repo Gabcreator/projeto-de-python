d86577b52e9ec6eaa617efdcc2c1454f0a080b37
Gabcreator <Jgabriel.carvalhojf@gmail.com>
2024-12-04 14:57:22 -0300

    trabalho

--- trabalhos/trabalho_tupla.py
+++ trabalhos/trabalho_tupla.py
@@ -1,29 +1,61 @@
+# iniciando
 import os
 os.system('cls')
 
-numeros1 = {1, 2, 3, 4, 5}
-numeros2 = {6, 7, 8, 9, 10}
-numeros3 = {11, 12, 13, 14, 15}
-numeros4 = {16, 17, 18, 19, 20}
+# Montando e mostrando a lista
+jogosJ = {'Sonic', 'Far Cry 4', 'Fortnite', 'Roblox', 'Minecraft', 'Blood Strike'}
+jogosR = {'Far cry 4', 'Battlefield',  'Fortnite', 'Red Dead Island'}
+jogosA = {'Roblox', 'lovers', 'plants vs zombies battle for neigboville', 'Minecraft'}
 
-juncao = numeros1.union(numeros2,numeros3)
-print(juncao)
+print(f'Os jogos favoritos de joão são: {jogosJ}')
+print('-'*70)
+print(f'Os jogos favoritos de Ronald são: {jogosR}')
+print('-'*70)
+print(f'Os jogos favoritos de Arthur primo de João são: {jogosA}')
+print('-'*70)
 
-iguais = juncao.intersection(numeros2)
-print(iguais)
+# Unindos as listas
+jogos_unidos = jogosJ.union(jogosR, jogosA)
+print(f'Os jogos favoritos de cada um em uma só lista é: {jogos_unidos}')
 
-diferente = numeros4.difference(numeros2)
-print(diferente)
+# Adicionando e removendo das listas
+jogosJ.add('Letal Company')
+print(f'João ganhou um jogo de Ronald e agora a lista dele é: {jogosJ}')
+print('-'*70)
 
-simetrica = numeros4.symmetric_difference(numeros3)
-print(simetrica)
+jogosJ.remove('Sonic')
+print(f'João tem muitos jogos de Sonic e então ele teve que os desinstalar e agora a lista ficou: {jogosJ}')
 
-intercessao = numeros4.intersection(numeros2)
-print(intercessao)
 
-subconjunto = numeros3.issubset(numeros2)
-print(subconjunto)
+print('-'*70)
+jogosR.add('Fallout')
+print(f'Ronald baixou mais um de seus jogos de tiro antigos e agora sua lista é: {jogosR}')
 
-descartar = numeros1.discard(numeros2)
-print(descartar)
+print('-'*70)
+jogosA.add('Escape the Gungeons')
+print(f'Arthur descobriu um novo jogo de dungeons e agora sua lista ficou: {jogosA}')
 
+print('-'*70)
+jogosA.remove('lovers')
+print(f'Arthur, para conseguir baixar este novo jogo teve que excluir um de seus jogos: lovers {jogosA}')
+
+print('-'*70)
+jogosR.remove('Red Dead Island')
+print(f'Ronald estava com a memória do computador cheia e ele teve que excluir um de seus jogos: Red Dead Island {jogosR}')
+
+print('-'*70)
+jogosJ.remove('Fortnite')
+print(f'João teve o mesmo problema que Ronald e então ele teve que excluir um de seus jogos: Fortnite {jogosJ}')
+
+print('-'*70)
+jogosJ.remove('Far Cry 4')
+print(f'João teve que excluir Far Cry 4 para poder jogar Blood strike com os amigos: {jogosJ}')
+
+# Terminando os códigos
+print('-'*70)
+print(F'A lista de João depois das mudanças ficou: {jogosJ}')
+print('-'*70)
+print(f'A lista de Ronald depois das mudanças ficou: {jogosR}')
+print('-'*70)
+print(f'A lista de Arthur depois das mudanças ficou: {jogosA}')
+print('-'*70)
